9aa0567da63ac926d0dd11be958e712fdc6e6d75
Voltzy9 <arthur.genthial@ynov.com>
2021-11-24 17:42:00 +0100

    Add price on Card list dish Create object Category

--- app/src/main/java/com/ynovlyon/ymenu/data/Dish.kt
+++ app/src/main/java/com/ynovlyon/ymenu/data/Dish.kt
@@ -2,7 +2,8 @@ package com.ynovlyon.ymenu.data
 
 data class Dish (
     val id: Int,
-    val title: String,
+    val name: String,
     val price: String,
     val dishesPictId: Int = 0,
+    val category: String,
 )
