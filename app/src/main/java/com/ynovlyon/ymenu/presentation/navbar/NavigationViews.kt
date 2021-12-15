aaf38fb2326f9b7dadbf3f5869fc8a8bc11886d0
Zakarya Tolba <zakarya.tolba@ynov.com>
2021-12-15 10:40:03 +0100

    Removed unused imports

--- app/src/main/java/com/ynovlyon/ymenu/presentation/navbar/NavigationViews.kt
+++ app/src/main/java/com/ynovlyon/ymenu/presentation/navbar/NavigationViews.kt
@@ -1,17 +1,9 @@
 package com.ynovlyon.ymenu
 
-import android.content.Intent
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.core.content.ContextCompat.startActivity
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.mlkit.vision.barcode.Barcode
-import com.google.mlkit.vision.barcode.BarcodeScannerOptions
-import com.google.mlkit.vision.barcode.BarcodeScanning
-import com.google.mlkit.vision.common.InputImage
 import com.ynovlyon.ymenu.camera.Camera
-import com.ynovlyon.ymenu.camera.CameraPreview
-import androidx.compose.runtime.Composable
 
 @Composable
 fun Greeting(name: String) {
