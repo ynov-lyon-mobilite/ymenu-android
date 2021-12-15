b92d7bb7da9badfe7b3e4cfb824d36cf64817d4c
Zakarya Tolba <zakarya.tolba@ynov.com>
2021-12-15 10:11:17 +0100

    Fixes after merge & removed comments

--- app/src/main/java/com/ynovlyon/ymenu/MainActivity.kt
+++ app/src/main/java/com/ynovlyon/ymenu/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.ynovlyon.ymenu
 
 import android.annotation.SuppressLint
-import android.graphics.Paint
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -32,33 +29,7 @@ import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
 import com.ynovlyon.ymenu.ui.theme.*
 import kotlinx.coroutines.launch
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.Text
@@ -66,7 +37,6 @@ import com.ynovlyon.ymenu.presentation.theme.YMenuTheme
 import com.ynovlyon.ymenu.presentation.theme.fonts
 
 import androidx.compose.runtime.*
-import androidx.loader.content.Loader
 import dev.burnoo.compose.rememberpreference.rememberBooleanPreference
 
 
@@ -104,7 +74,7 @@ class MainActivity : ComponentActivity() {
                     // val preferences = getSharedPreferences("my_preferences", MODE_PRIVATE)
 
                     //if (!preferences.getBoolean("onboarding_complete", false)) {
-                      //  OnBoardScreen { isOnboardingCompleted = true }
+                    //  OnBoardScreen { isOnboardingCompleted = true }
 
                     //}
                 }
@@ -112,182 +82,185 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-@SuppressLint("CoroutineCreationDuringComposition")
-@ExperimentalPagerApi
-@Composable
-fun OnBoardScreen(onCompleted: () -> Unit) {
-    val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
-    val onBoardViewModel : OnBoardViewModel = viewModel()
-    val context = LocalContext.current
-    val currentPage = onBoardViewModel.currentPage.collectAsState()
+    @SuppressLint("CoroutineCreationDuringComposition")
+    @ExperimentalPagerApi
+    @Composable
+    fun OnBoardScreen(onCompleted: () -> Unit) {
+        val scaffoldState = rememberScaffoldState()
+        val scope = rememberCoroutineScope()
+        val onBoardViewModel: OnBoardViewModel = viewModel()
+        val context = LocalContext.current
+        val currentPage = onBoardViewModel.currentPage.collectAsState()
 
-    Toast.makeText(context, "${currentPage.value}", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "${currentPage.value}", Toast.LENGTH_SHORT).show()
 
-    val pagerState = rememberPagerState(
-        pageCount = onBoardItem.size,
-        initialOffscreenLimit = 2,
-        initialPage = 0,
-        infiniteLoop = false
-    )
+        val pagerState = rememberPagerState(
+            pageCount = onBoardItem.size,
+            initialOffscreenLimit = 2,
+            initialPage = 0,
+            infiniteLoop = false
+        )
 
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        scaffoldState = scaffoldState
-    ) {
-        Surface(
-            modifier = Modifier.fillMaxSize()
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            scaffoldState = scaffoldState
         ) {
-            scope.launch {
-                pagerState.animateScrollToPage(
-                    page = currentPage.value
-                )
-            }
-
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.White)
+            Surface(
+                modifier = Modifier.fillMaxSize()
             ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    HorizontalPager(
-                        state = pagerState
-                    ) { page ->
-                        Column(
-                            modifier = Modifier
-                                .padding(top = 65.dp)
-                                .fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Image(
-                                painter = painterResource(id = onBoardItem[page].image),
-                                contentDescription = "OnBoardImage",
-                                modifier = Modifier
-                                    .size(250.dp)
-                            )
-
-                            Text(
-                                text = onBoardItem[page].title,
-                                modifier = Modifier
-                                    .padding(top = 50.dp),
-                                textAlign = TextAlign.Center,
-                                color = Orange200,
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 40.sp
-                            )
-
-                            Text(
-                                text = onBoardItem[page].desc,
-                                modifier = Modifier
-                                    .padding(30.dp),
-                                color = Color.Black,
-                                fontSize = 16.sp,
-                                textAlign = TextAlign.Center
-                            )
-
-                            if (pagerState.currentPage != 2) {
-                                OutlinedButton (
-                                    onClick = {
-                                        onBoardViewModel.setCurrentPage(pagerState.currentPage + 1)
-                                    },
-                                    colors = ButtonDefaults.buttonColors(
-                                        backgroundColor = Orange200,
-                                        contentColor = Color.White),
-                                    shape = RoundedCornerShape(45.dp)
-                                ) {
-                                    Text(
-                                        text = "Suivant",
-                                        fontFamily = fonts,
-                                        color = Color.White,
-                                        modifier = Modifier
-                                            .padding(
-                                                vertical = 8.dp,
-                                                horizontal = 40.dp
-                                            )
-                                    )
-                                }
-                            } else {
-                                OutlinedButton(
-                                    onClick = onCompleted,
-                                    colors = ButtonDefaults.buttonColors(
-                                        backgroundColor = Orange200,
-                                        contentColor = Color.White),
-                                    shape = RoundedCornerShape(45.dp)
-                                ) {
-                                    Text(
-                                        text = "C'est parti !",
-                                        fontFamily = fonts,
-                                        modifier = Modifier
-                                            .padding(
-                                                vertical = 8.dp,
-                                                horizontal = 40.dp
-                                            ),
-                                        color = Color.White
-                                    )
-                                }
-                            }
-                        }
-                    }
-
-                    PagerIndicator(onBoardItem.size, pagerState.currentPage)
+                scope.launch {
+                    pagerState.animateScrollToPage(
+                        page = currentPage.value
+                    )
                 }
 
                 Box(
                     modifier = Modifier
-                        .align(Alignment.BottomCenter)
+                        .fillMaxWidth()
+                        .background(Color.White)
                 ) {
-                    Row(
-                        modifier = Modifier
-                            .padding(bottom = 20.dp)
-                            .fillMaxWidth(),
-                        horizontalArrangement = if (pagerState.currentPage != 2 ) {
-                            Arrangement.SpaceBetween
-                        } else {
-                            Arrangement.Center
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        HorizontalPager(
+                            state = pagerState
+                        ) { page ->
+                            Column(
+                                modifier = Modifier
+                                    .padding(top = 65.dp)
+                                    .fillMaxWidth(),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Image(
+                                    painter = painterResource(id = onBoardItem[page].image),
+                                    contentDescription = "OnBoardImage",
+                                    modifier = Modifier
+                                        .size(250.dp)
+                                )
+
+                                Text(
+                                    text = onBoardItem[page].title,
+                                    modifier = Modifier
+                                        .padding(top = 50.dp),
+                                    textAlign = TextAlign.Center,
+                                    color = Orange200,
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 40.sp
+                                )
+
+                                Text(
+                                    text = onBoardItem[page].desc,
+                                    modifier = Modifier
+                                        .padding(30.dp),
+                                    color = Color.Black,
+                                    fontSize = 16.sp,
+                                    textAlign = TextAlign.Center
+                                )
+
+                                if (pagerState.currentPage != 2) {
+                                    OutlinedButton(
+                                        onClick = {
+                                            onBoardViewModel.setCurrentPage(pagerState.currentPage + 1)
+                                        },
+                                        colors = ButtonDefaults.buttonColors(
+                                            backgroundColor = Orange200,
+                                            contentColor = Color.White
+                                        ),
+                                        shape = RoundedCornerShape(45.dp)
+                                    ) {
+                                        Text(
+                                            text = "Suivant",
+                                            fontFamily = fonts,
+                                            color = Color.White,
+                                            modifier = Modifier
+                                                .padding(
+                                                    vertical = 8.dp,
+                                                    horizontal = 40.dp
+                                                )
+                                        )
+                                    }
+                                } else {
+                                    OutlinedButton(
+                                        onClick = onCompleted,
+                                        colors = ButtonDefaults.buttonColors(
+                                            backgroundColor = Orange200,
+                                            contentColor = Color.White
+                                        ),
+                                        shape = RoundedCornerShape(45.dp)
+                                    ) {
+                                        Text(
+                                            text = "C'est parti !",
+                                            fontFamily = fonts,
+                                            modifier = Modifier
+                                                .padding(
+                                                    vertical = 8.dp,
+                                                    horizontal = 40.dp
+                                                ),
+                                            color = Color.White
+                                        )
+                                    }
+                                }
+                            }
                         }
-                    ) {}
+
+                        PagerIndicator(onBoardItem.size, pagerState.currentPage)
+                    }
+
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .padding(bottom = 20.dp)
+                                .fillMaxWidth(),
+                            horizontalArrangement = if (pagerState.currentPage != 2) {
+                                Arrangement.SpaceBetween
+                            } else {
+                                Arrangement.Center
+                            }
+                        ) {}
                     }
                 }
             }
         }
     }
 
-@Composable
-fun PagerIndicator(size: Int, currentPage: Int) {
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier.padding(top = 60.dp)
-    ) {
-        repeat(size) {
-            IndicateIcon(
-                isSelected = it == currentPage
-            )
+    @Composable
+    fun PagerIndicator(size: Int, currentPage: Int) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.padding(top = 60.dp)
+        ) {
+            repeat(size) {
+                IndicateIcon(
+                    isSelected = it == currentPage
+                )
+            }
         }
     }
-}
 
-@Composable
-fun IndicateIcon(isSelected: Boolean) {
-    val width = animateDpAsState(
-        targetValue = if (isSelected) 25.dp else 10.dp
-    )
+    @Composable
+    fun IndicateIcon(isSelected: Boolean) {
+        val width = animateDpAsState(
+            targetValue = if (isSelected) 25.dp else 10.dp
+        )
 
-    Box(
-        modifier = Modifier
-            .padding(2.dp)
-            .height(10.dp)
-            .width(width.value)
-            .clip(CircleShape)
-            .background(
-                if (isSelected) {
-                    Orange200
-                } else {
-                    Color.Gray.copy(alpha = 0.5f)
-                }
-            )
-    )
+        Box(
+            modifier = Modifier
+                .padding(2.dp)
+                .height(10.dp)
+                .width(width.value)
+                .clip(CircleShape)
+                .background(
+                    if (isSelected) {
+                        Orange200
+                    } else {
+                        Color.Gray.copy(alpha = 0.5f)
+                    }
+                )
+        )
+    }
 }
 
 @Composable
