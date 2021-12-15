f37d9403cb117c31b1943bb6be5f2c5f31292e7e
David A <d.ananda974@gmail.com>
2021-12-15 09:26:05 +0100

    QR code scanner in navbar

---
+++ app/src/main/java/com/ynovlyon/ymenu/camera/BarcodeScanningActivity.kt
@@ -0,0 +1,84 @@
+package com.ynovlyon.ymenu.camera
+
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Rect
+import android.graphics.RectF
+import android.widget.Toast
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+
+class QrCodeAnalyzer(
+    private val context: Context,
+    private val qrCodeBoxView: QrCodeBoxView,
+) : ImageAnalysis.Analyzer {
+
+    /**
+     * This parameters will handle preview box scaling
+     */
+    private var scaleX = 1f
+    private var scaleY = 1f
+
+    private fun translateX(x: Float) = x * scaleX
+    private fun translateY(y: Float) = y * scaleY
+
+    private fun adjustBoundingRect(rect: Rect) = RectF(
+        translateX(rect.left.toFloat()),
+        translateY(rect.top.toFloat()),
+        translateX(rect.right.toFloat()),
+        translateY(rect.bottom.toFloat())
+    )
+
+    @SuppressLint("UnsafeOptInUsageError")
+    override fun analyze(image: ImageProxy) {
+        val img = image.image
+        if (img != null) {
+            // Update scale factors
+            scaleX = 500 / img.height.toFloat()
+            scaleY = 500 / img.width.toFloat()
+            println("SCAAALE" + scaleX)
+            println("SCAAALE" + scaleY)
+
+            val inputImage = InputImage.fromMediaImage(img, image.imageInfo.rotationDegrees)
+
+            // Process image searching for barcodes
+            val options = BarcodeScannerOptions.Builder()
+                .build()
+
+            val scanner = BarcodeScanning.getClient(options)
+
+            scanner.process(inputImage)
+                .addOnSuccessListener { barcodes ->
+                    if (barcodes.isNotEmpty()) {
+                        for (barcode in barcodes) {
+                            // Handle received barcodes...
+                            Toast.makeText(
+                                context,
+                                "Value: " + barcode.rawValue,
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                            // Update bounding rect
+                            barcode.boundingBox?.let { rect ->
+                                qrCodeBoxView.setRect(
+                                    adjustBoundingRect(
+                                        rect
+                                    )
+                                )
+                            }
+                        }
+                    } else {
+                        // Remove bounding rect
+                        qrCodeBoxView.setRect(RectF())
+                    }
+                }
+                .addOnFailureListener { }
+        }
+
+        image.close()
+    }
+}
--- app/src/main/java/com/ynovlyon/ymenu/presentation/navbar/NavigationViews.kt
+++ app/src/main/java/com/ynovlyon/ymenu/presentation/navbar/NavigationViews.kt
@@ -1,7 +1,17 @@
 package com.ynovlyon.ymenu
 
+import android.content.Intent
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.core.content.ContextCompat.startActivity
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+import com.ynovlyon.ymenu.camera.Camera
+import com.ynovlyon.ymenu.camera.CameraPreview
+
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
@@ -11,10 +21,14 @@ fun Greeting(name: String) {
 fun Menu() {
     Greeting("menu")
 }
-
+@ExperimentalPermissionsApi
 @Composable
 fun QrCode(){
-    Greeting("qrcode")
+    Camera()
+}
+
+@Composable
+fun iui(){
 }
 
 @Composable
