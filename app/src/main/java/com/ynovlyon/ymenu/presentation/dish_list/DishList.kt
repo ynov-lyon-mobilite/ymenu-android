b7ab327bb272fc5e4ffa8a6be4fbaefd44a30f9b
arthur <arthur.genthial@ynov.com>
2022-02-16 10:59:00 +0100

    add carrousel and list dish

--- app/src/main/java/com/ynovlyon/ymenu/presentation/dish_list/DishList.kt
+++ app/src/main/java/com/ynovlyon/ymenu/presentation/dish_list/DishList.kt
@@ -2,9 +2,6 @@ package com.ynovlyon.ymenu.presentation.dish_list
 
 import android.graphics.Color
 import android.provider.ContactsContract
-import android.util.Log
-import android.util.Log.getStackTraceString
-import android.util.Log.i
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -14,27 +11,29 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.ScrollableTabRow
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.google.accompanist.pager.*
 import com.ynovlyon.ymenu.DishListItem
 import com.ynovlyon.ymenu.data.DataProvider
-import com.ynovlyon.ymenu.data.DataProvider.categoryList
-import com.ynovlyon.ymenu.data.DishCategory
-import android.util.Log.v as v1
+import com.ynovlyon.ymenu.data.Dish
+import com.ynovlyon.ymenu.presentation.theme.YMenuTheme
+import com.ynovlyon.ymenu.ui.theme.Orange200
+import kotlinx.coroutines.launch
+
 
 @ExperimentalPagerApi
 
 @Composable
-fun DishList() {
-    //val tabIndex = remember { mutableStateOf(0)}
+fun DishList(navController: NavController) {
     val tabTitles =  DataProvider.categoryList
     val pagerState = rememberPagerState()
+    val scope = rememberCoroutineScope()
+    val dishes = DataProvider.dishesList
     Column() {
         ScrollableTabRow(selectedTabIndex = pagerState.currentPage,
             indicator = { tabPositions ->
@@ -47,32 +46,39 @@ fun DishList() {
             }) {
             tabTitles.forEachIndexed { index, title ->
                 Tab(selected = pagerState.currentPage == index,
-                    onClick = { pagerState.targetPage
-//                        i("cc", "add")
-//                        getStackTraceString(pagerState.targetPage)
+                    onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
                     },
                     text =  { Text(title.name) })
+
             }
         }
         VerticalPager(
             state = pagerState,
             count = tabTitles.size,
         ) { tabIndex ->
-            Text( tabIndex.toString(),
-            modifier = Modifier 
-                .fillMaxSize()
-              // .background(Color.white)
-            )
-            
+            LazyColumn(
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+            ) {
+                items(
+                    items = dishes,
+                    itemContent = {
+                        DishListItem(dish = it, navController = navController)
+                    },
+                )
+            }
         }
     }
 }
 
 
 
-@Composable
-fun CategoryList(selectedTabIndex: Int) {
-    TabRow(selectedTabIndex = selectedTabIndex) {
 
-    }
+@Composable
+fun CategoryList(selectedTabIndex: Int, dish: Dish) {
+//    TabRow(selectedTabIndex = selectedTabIndex) {
+//        DishListItem(dish = dish)
+//    }
 }
