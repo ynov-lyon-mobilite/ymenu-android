77c43c94dd2792cc81371a26ff30423b0a33b090
Zakarya Tolba <zakarya.tolba@ynov.com>
2022-05-10 14:36:41 +0200

    Added new dish list design

--- app/src/main/java/com/ynovlyon/ymenu/presentation/dish_list/DishList.kt
+++ app/src/main/java/com/ynovlyon/ymenu/presentation/dish_list/DishList.kt
@@ -1,11 +1,11 @@
 package com.ynovlyon.ymenu.presentation.dish_list
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Upload
@@ -65,13 +65,22 @@ fun DishList(navController: NavController, dishes: List<DishModel>, restaurant: 
         content = {
             LazyColumn(
                 contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                modifier = Modifier.padding(bottom = 80.dp)
             ) {
                 itemsIndexed(dishes) { index, dish ->
-                    DishListItem(dish = dish, navController = navController) {
+                    DishListItem(dish = dish) {
                         navController.currentBackStackEntry?.savedStateHandle?.set("dish", dish)
                         navController.navigate("dishDetail")
                     }
+                    Card(
+                        modifier = Modifier
+                            .padding(horizontal = 5.dp, vertical = 5.dp)
+                            .fillMaxWidth(),
+                        elevation = 2.dp,
+                        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+                    ) {}
                 }
+
             }
         }
     )
--- app/src/main/java/com/ynovlyon/ymenu/presentation/dish_list/DishListItem.kt
+++ app/src/main/java/com/ynovlyon/ymenu/presentation/dish_list/DishListItem.kt
@@ -1,32 +1,36 @@
 package com.ynovlyon.ymenu
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import com.google.gson.Gson
 import com.ynovlyon.ymenu.data.model.DishModel
+import com.ynovlyon.ymenu.presentation.theme.YmenuOrange
+import com.ynovlyon.ymenu.presentation.theme.YmenuOrangeAlt
 
 @Composable
-fun DishListItem(dish: DishModel, navController: NavController, onClick: () -> Unit) {
-    fun navigateToDish(dish: DishModel) {
-        val dishJson = Gson().toJson(dish)
-        navController.navigate("dish/$dishJson")
-    }
-
+fun DishListItem(dish: DishModel, onClick: () -> Unit) {
     Card(
         modifier = Modifier
             .padding(horizontal = 5.dp, vertical = 5.dp)
@@ -34,11 +38,9 @@ fun DishListItem(dish: DishModel, navController: NavController, onClick: () -> U
             .clickable(
                 onClick = {
                     onClick()
-                    // navigateToDish(dish = dish)
                 }
             ),
         elevation = 2.dp,
-//        backgroundColor = Color.White,
         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
     ) {
         Row(
@@ -48,20 +50,24 @@ fun DishListItem(dish: DishModel, navController: NavController, onClick: () -> U
             Column(
                 modifier = Modifier
                     .padding(20.dp)
-                    .width(100.dp),
+                    .width(300.dp),
             ) {
-                Text(text = dish.name, fontWeight = FontWeight.Bold)
+                Text(text = dish.name, fontWeight = FontWeight.Bold, color = YmenuOrange, fontSize = 15.sp)
                 Text(
-                    text = "View Detail",
-                    fontStyle = FontStyle.Italic,
-                    modifier = Modifier.padding(top = 5.dp)
+                    text = dish.ingredients.joinToString(separator = ", "),
+                    fontWeight = FontWeight.W300,
+                    fontSize = 11.sp,
+                    modifier = Modifier.padding(top = 5.dp),
+                    color = Color.Gray
                 )
-            }
-            Column(
-                modifier = Modifier
-                    .padding(20.dp),
-            ) {
-                Text(text = (dish.price).toString(), fontWeight = FontWeight.Bold)
+                Card(
+                    elevation = 2.dp,
+                    shape = RoundedCornerShape(9.dp),
+                    backgroundColor = YmenuOrange,
+                    modifier = Modifier.padding(top = 10.dp)
+                ) {
+                    Text(text ="${dish.price}0 €", fontSize = 12.sp,fontWeight = FontWeight.Bold, color = Color.White, modifier = Modifier.padding(vertical = 5.dp, horizontal = 7.dp))
+                }
             }
         }
     }
@@ -70,14 +76,19 @@ fun DishListItem(dish: DishModel, navController: NavController, onClick: () -> U
 
 @Composable
 fun DishImage(dish: DishModel) {
+        Image(
+            painter = rememberAsyncImagePainter(dish.url_logo),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(8.dp)
+                .shadow(
+                    elevation = 5.dp,
+                    shape = RoundedCornerShape(13.dp),
+                    clip = true
+                )
+                .size(height = 130.dp, width = 120.dp)
+                .clip(RoundedCornerShape(corner = CornerSize(13.dp)))
 
-    Image(
-        painter = rememberAsyncImagePainter(dish.url_logo),
-        contentDescription = null,
-        contentScale = ContentScale.Crop,
-        modifier = Modifier
-            .padding(8.dp)
-            .size(84.dp)
-            .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
-    )
+        )
 }
